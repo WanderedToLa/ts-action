6728fffb374719e2c1e39d2e1adc829b75d423fc
WanderedToLa <rladntjr9808@gmail.com>
2024-01-25 14:17:52 +0900

    aa

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { wait } from './wait'
 export async function run(): Promise<void> {
   try {
     const ms: string = core.getInput('milliseconds')
-
+    core.info('Process 1')
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
     core.debug(`Waiting ${ms} milliseconds ...`)
 
